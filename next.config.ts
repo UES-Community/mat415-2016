b445b518ef6c949d0386b5386035fcf796225420
Cristian Antonio Escalante Hernandez <heac.creative@gmail.com>
2026-07-19 13:07:40 -0600

    ci: configure GitHub Pages deployment workflow using Node 24 and pnpm

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "mat415-2016";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
